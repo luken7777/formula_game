96131067be4e7f061c0897b2eb990e8139916008
luken7777 <35407779+luken7777@users.noreply.github.com>
2018-11-19 12:26:07 +0000

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,1 +1,15 @@
 
+<?php
+
+//Credentials in order to connect to database
+$servername = "localhost";
+$username = "root";
+$password = "somepass";
+$dbName = "formula";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
